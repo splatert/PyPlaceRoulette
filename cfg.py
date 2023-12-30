a6ac18f9f8fb429cf8078694a03d5706886f0907
splatert <82643571+splatert@users.noreply.github.com>
2023-12-30 14:54:20 -0500

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -3,9 +3,8 @@
 
 settings = {
    
-    # Replace COOKIE with your ROBLOSECURITY key for PyPlaceRoulette to make web requests. Do NOT share the key with anyone.
-    # Guess Roblox requires it once? Because I was able to keep making requests after erasing the key.
-    # Though before I passed my own key, I would only get '403 Forbidden' errors.
+    # Replace COOKIE with your ROBLOSECURITY key in order to successfully make web requests or else Roblox will think that you're a non-user.
+    # Do NOT share the key with anyone.
 
     "SecurityKey": "COOKIE",
     
