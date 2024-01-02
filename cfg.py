956e2fa87eb2ac9dc2ef992787b398148d867118
splatert <82643571+splatert@users.noreply.github.com>
2024-01-02 16:23:49 -0500

    Add files via upload

--- cfg.py
+++ cfg.py
@@ -3,18 +3,23 @@
 
 settings = {
    
-    # Replace COOKIE with your ROBLOSECURITY key in order to successfully make web requests or else Roblox will think that you're a non-user.
-    # Do NOT share the key with anyone.
+    # Replace COOKIE with your ROBLOSECURITY key for PyPlaceRoulette to make web requests. Do NOT share the key with anyone.
+    # It is required so that you don't end up getting 'Unauthorized' errors.
 
-    "SecurityKey": "COOKIE",
-    
+    "SecurityKey": "COOKIE" 
     # Skip starter (new user) places
     "SkipStarterPlaces": True,
 
     # View place webpage in your browser
     "ViewPlaceInWebBrowser": True,
     
-    # work in progress
+    # Skip places that you cannot access
     "SkipPrivatePlaces": True,
+
+    # Smallest ID in random ID generation (Default: 250000)
+    "minID": 250000,
+
+    # Largest ID in random ID generation (Default: 90123456)
+    "maxID": 90123456,
 
 }
